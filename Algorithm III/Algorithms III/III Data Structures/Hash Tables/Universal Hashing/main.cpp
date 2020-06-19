404f577999a477949865ffc47ca658db71a9f1ee
Alexis Quinto <alexisgquinto@gmail.com>
2020-06-19 18:58:31 +0800

    Additional universal hash function collection for keys with integer domain

--- Algorithm III/Algorithms III/III Data Structures/Hash Tables/Universal Hashing/main.cpp
+++ Algorithm III/Algorithms III/III Data Structures/Hash Tables/Universal Hashing/main.cpp
@@ -8,46 +8,90 @@
 
 #include <iostream>
 #include <stdlib.h>
+#include <math.h>
+#include <stdlib.h>
 
-#define R 4 // length of vector based from the key
-#define M 701 // m is prime which is the size of hash table
+// for hashFunc()
+const int r = 4;
+int m1 = 701;
+int A[r] = {
+    rand() % m1,
+    rand() % m1,
+    rand() % m1,
+    rand() % m1
+};
 
-using namespace std;
+// for hashFunc2()
+int p = (pow(2, 51) - 1);
+int b = (rand() % p);
+int a = ((rand() % (p-1)) + 1);
+int m2 = 10000;
 
-int hashFunc(int a[R], int k[R]) {
+// R is the size of vector based from the key
+// M is the size of the hash table which is a prime
+int hashFunc(int A[r], int K[r]) {
     int h = 0;
     for (int i = 0; i < 4; ++i) {
-        h += (a[i] * k[i]) % M;
+        h += (A[i] * K[i]) % m1;
     }
-    return h % M;
+    return h % m1;
+}
+
+// Use this hash function if the key universe U is an integer domain that can be fit into machine word
+// U => [u] = {0,1,...,u-1} and [u] <= 2^w (where w = 64, the word length)
+// For this example: we will assume that all given key k could have a maximum value of 2^50
+long long hashFunc2(long long k) {
+    // p is a prime number that could cover all possible keys which are in the range from 0 to p-1, inclusive
+    // use Mersenne Prime Number => (2 ^ p) - 1
+    // b should come from the set of {0,1,...,p-1}
+    // a should come from the set of {1,2,...,p-1}
+    // m is the size of hash table which can be arbitrary and not necessarily a prime (p > m)
+
+    long long h = (((a * k) + b) % p) % m2;
+    
+    return h;
+}
+
+// Radix notation where keys are viewed in base r (ex. r = 2^5 enough to represents 26 chars ~ a to z)
+// Be careful not to exceed the maximum value of long long (2^64)
+// For this example:
+// all given str will be in lowercase (ie. "alexis")
+// strLen is the length of the str
+// rbits is the number of bits that could represent a subkey
+long long radixNotation(char* str, int strLen, int rbits) {
+    int index = strLen - 1;
+    long long h = 0L;
+    long long shift = 0L;
+    while (index >= 0) {
+        h += (str[index--] - 'a') << shift;
+        shift += rbits;
+    }
+    return h;
 }
 
 int main(int argc, const char * argv[]) {
-    int a[R] = {
-        rand() % M,
-        rand() % M,
-        rand() % M,
-        rand() % M
-    };
-    
-    // keys are
-    int keys[R][R] {
+    // keys should viewed as vectors of subkeys
+    // subkeys should be treated as base k (for this example: k = 255)
+    int keys[r][r] {
         { 107, 105, 136, 147 }, // sample IP 1
         { 107, 105, 136, 148 }, // sample IP 2
         { 107, 105, 136, 149 }, // sample IP 3
         { 107, 105, 136, 147 } // sample IP 4
     };
     
-    int h1 = hashFunc(a, keys[0]);
-    int h2 = hashFunc(a, keys[1]);
-    int h3 = hashFunc(a, keys[2]);
-    int h4 = hashFunc(a, keys[3]);
-    
     std::cout << "Hello, World!\n";
-    std::cout << "Hash value of IP 1: " << h1 << '\n';
-    std::cout << "Hash value of IP 2: " << h2 << '\n';
-    std::cout << "Hash value of IP 3: " << h3 << '\n';
-    std::cout << "Hash value of IP 4: " << h4 << '\n';
+    std::cout << "Hash function 1: " << '\n';
+    std::cout << "Hash value of IP 1: " << hashFunc(A, keys[0]) << '\n';
+    std::cout << "Hash value of IP 2: " << hashFunc(A, keys[1]) << '\n';
+    std::cout << "Hash value of IP 3: " << hashFunc(A, keys[2]) << '\n';
+    std::cout << "Hash value of IP 4: " << hashFunc(A, keys[3]) << '\n';
+
+    std::cout << "Hash function 2: " << '\n';
+    long long k1 = radixNotation("alexis", 6, 5);
+    long long k2 = radixNotation("alexir", 6, 5);
+    std::cout << "Hash value of 'alexis': key["<< k1 << "] = " << hashFunc2(k1) << '\n';
+    std::cout << "Hash value of 'alexir': key["<< k2 << "] = " << hashFunc2(k2) << '\n';
+    std::cout << "Hash value of 'alexis': key["<< k1 << "] = " << hashFunc2(k1) << '\n';
     
     return 0;
 }
