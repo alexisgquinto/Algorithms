9adece4dd6f3b2297fea766eb9012a4e2e151a95
Alexis Quinto <alexisgquinto@gmail.com>
2020-07-01 00:10:12 +0800

    Disjoint sets

--- Algorithm III/Algorithms III/IV Advanced Data Structures/Disjoint Sets/main.cpp
+++ Algorithm III/Algorithms III/IV Advanced Data Structures/Disjoint Sets/main.cpp
@@ -8,22 +8,25 @@
 
 #include <iostream>
 
-class Node {
+#define NIL 0
+#define MAX_V_SIZE 10
+
+class Vertex {
 public:
     int key;
     int rank;
-    Node* parent;
-    Node(int k) {
+    Vertex* parent;
+    Vertex(int k) {
         key = k;
     }
 };
 
-void makeSet(Node* x) {
+void makeSet(Vertex* x) {
     x->parent = x;
     x->rank = 0;
 }
 
-void linkSet(Node* x, Node* y) {
+void linkSet(Vertex* x, Vertex* y) {
     if (x->rank > y->rank) {
         y->parent = x;
     } else {
@@ -33,18 +36,61 @@ void linkSet(Node* x, Node* y) {
     }
 }
 
-Node* findSet(Node* x) {
+Vertex* findSet(Vertex* x) {
     if (x != x->parent)
         x->parent = findSet(x->parent);
     return x->parent;
 }
 
-void joinSet(Node* x, Node* y) {
+void joinSet(Vertex* x, Vertex* y) {
     linkSet(findSet(x), findSet(y));
+}
+
+class Graph {
+public:
+    int Adj[MAX_V_SIZE + 1][MAX_V_SIZE + 1] = {};
+    Vertex* V[MAX_V_SIZE + 1] = {};
+};
+
+void addVertex(Graph* G, int u) {
+    if (G->V[u] == NIL)
+        G->V[u] = new Vertex(u);
+}
+
+void addEdge(Graph* G, int u, int v) {
+    G->Adj[u][v] = 1;
+    G->Adj[v][u] = 1;
 }
 
 int main(int argc, const char * argv[]) {
     std::cout << "Hello, World!\n";
+    
+    Graph* G = new Graph();
+    
+    for (int i = 1; i <= MAX_V_SIZE; ++i) {
+        addVertex(G, i);
+        makeSet(G->V[i]);
+    }
+    
+    addEdge(G, 1, 2);
+    addEdge(G, 2, 3);
+    addEdge(G, 3, 1);
+    addEdge(G, 2, 4);
+    addEdge(G, 5, 6);
+    addEdge(G, 5, 7);
+    addEdge(G, 8, 9);
+    
+    for (int i = 1; i <= MAX_V_SIZE; ++i)
+        for (int j = 1; j <= MAX_V_SIZE; ++j)
+            if (G->Adj[i][j] == 1)
+                if (findSet(G->V[i]) != findSet(G->V[j]))
+                    joinSet(G->V[i], G->V[j]);
+    
+    std::cout << "Connected Components: " << '\n';
+    for (int i = 1; i <= MAX_V_SIZE; ++i) {
+        Vertex* u = G->V[i];
+        std::cout << "Root from the set of vertex [" << i << "] is [" << findSet(u)->key << "]\n";
+    }
     
     return 0;
 }
